70d108da2b8c4c49a4837a53b1246077ea8aceec
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2022-12-02 12:58:45 +0100

    update DBManager.py

--- PycharmProject/DBManager.py
+++ PycharmProject/DBManager.py
@@ -8,17 +8,28 @@ class DBManager:
         pass
 
     @staticmethod
-    def insert_record(self, dataframe, path_db, table):
+    def insert_record(dataframe, path_db, table):
         conn = sqlite3.connect(path_db, timeout=15)
         dataframe.to_sql(table, conn, if_exist="append", index=False)
 
     @staticmethod
-    def execute_query(self):
-        pass
+    def execute_query(path_db, query):
+        conn = sqlite3.connect(path_db)
+        try:
+            res = pd.read_sql(query, conn)
+            conn.commit()
+        except:
+            print("Exeption in query - special char")
+        conn.close()
+        return res
 
     @staticmethod
-    def delete_table(self):
-        pass
+    def delete_table(path_db, table):
+        conn = sqlite3.connect(path_db)
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS " + table)
+        conn.commit()
+        conn.close()
 
     @staticmethod
     def create_table(self):
