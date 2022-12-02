70b093f6d1804057790dd70954b6bb5304f9eaf0
veronicator <80213270+veronicator@users.noreply.github.com>
2022-12-02 13:01:20 +0100

    definition create_table and update_table

--- PycharmProject/DBManager.py
+++ PycharmProject/DBManager.py
@@ -32,10 +32,23 @@ class DBManager:
         conn.close()
 
     @staticmethod
-    def create_table(self):
-        pass
+    def create_table(self, path_db, query):
+        conn = sqlite3.connect(path_db)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
 
     @staticmethod
-    def update_table(self):
-        pass
+    def update_table(self, path_db, query):
+        conn = sqlite3.connect(path_db)
+        try:
+            cur = conn.cursor()
+            cur.execute(query)
+            conn.commit()
+        except:
+            print("Query exception")
+        conn.close()
+
+
 
