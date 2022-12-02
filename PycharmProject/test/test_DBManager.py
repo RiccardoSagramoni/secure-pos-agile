fab55655fdb126e9aee635259cb4474ee32217ca
veronicator <80213270+veronicator@users.noreply.github.com>
2022-12-02 16:49:20 +0100

    fixed primary key declaration and test

--- PycharmProject/test/test_DBManager.py
+++ PycharmProject/test/test_DBManager.py
@@ -7,7 +7,7 @@ path_db = "test.db"
 
 def test_create_table():
     try:
-        query = "CREATE TABLE example (ex_id TEXT PRIMARY KEY, name TEXT, lastName TEXT)"
+        query = "CREATE TABLE example (ex_id TEXT PRIMARY KEY UNIQUE, name TEXT, lastName TEXT)"
         db.create_table(path_db, query)
         assert True
     except:
@@ -15,7 +15,7 @@ def test_create_table():
 
 
 def test_insert_record():
-    data = {"ex_id": "1",
+    data = {"ex_id": "2",
             "name": "team",
             "lastName": "cammeo"}
     df = pd.DataFrame(data, index=[0])
@@ -41,7 +41,7 @@ def test_execute_query():
 
 def test_update_table():
     try:
-        query = "UPDATE example SET name='pippo' WHERE ex_id='1'"
+        query = "UPDATE example SET name='pippo' WHERE ex_id='2'"
         db.update_table(path_db, query)
         query = "SELECT * FROM example"
         res = db.execute_query(path_db, query)
