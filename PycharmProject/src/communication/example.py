146587bdd54acb42e0985369454377f380059bb2
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-12-05 18:42:28 +0100

    Change example for communication module

--- PycharmProject/src/communication/example.py
+++ PycharmProject/src/communication/example.py
@@ -2,17 +2,18 @@ from communication import RestServer
 from communication.api import EchoApi, GetFileApi, ManageJsonApi, ReadCSVApi
 
 if __name__ == "__main__":
+    # Instantiate server
     server = RestServer()
+    # Add APIs
     server.api.add_resource(EchoApi, "/")
-
     server.api.add_resource(GetFileApi,
                             "/get/<filename>",
                             resource_class_kwargs={'base_path': '../../data/'})
-
-    server.api.add_resource(ManageJsonApi, "/json/<filename>",
+    server.api.add_resource(ManageJsonApi,
+                            "/json/<filename>",
                             resource_class_kwargs={'base_path': '../../data/'})
-
-    server.api.add_resource(ReadCSVApi, "/csv/<filename>",
+    server.api.add_resource(ReadCSVApi,
+                            "/csv/<filename>",
                             resource_class_kwargs={'base_path': '../../data/'})
-
+    # Start server
     server.run(debug=True)
