316ddda6d311ea2815a4be6dcbcdd02484a82291
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-12-06 12:28:45 +0100

    JSON validation against schema

---
+++ PycharmProject/src/json_utility/__init__.py
@@ -0,0 +1,17 @@
+import jsonschema
+
+from jsonschema import validate
+
+
+def validate_json(json_data, schema) -> bool:
+    """
+    Validate a json object against a json schema.
+    :param json_data: json object
+    :param schema: json schema
+    :return: True if json object is valid, False otherwise.
+    """
+    try:
+        validate(instance=json_data, schema=schema)
+    except jsonschema.exceptions.ValidationError:
+        return False
+    return True
