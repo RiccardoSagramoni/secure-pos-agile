5cf8bcb0db4de98f2d66dd1da3e2421183b71f29
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-12-06 11:40:55 +0100

    Refactor of database package
    - Changed package structure
    - Edit pytest
    - Refactor function names
    - Refactor handling of connection

---
+++ PycharmProject/src/database/__init__.py
@@ -0,0 +1,50 @@
+from typing import Union, Iterator
+
+import os
+import pandas as pd
+import sqlite3
+
+
+class DBManager:
+
+    def __init__(self, path_db: str):
+        self.path_db = path_db
+
+    def create_table(self, query: str) -> None:
+        with sqlite3.connect(self.path_db) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+
+    def insert(self, dataframe: pd.DataFrame, table: str) -> bool:
+        with sqlite3.connect(self.path_db, timeout=15) as conn:
+            res = dataframe.to_sql(table, conn, if_exists="append", index=False)
+            return bool(res)
+
+    def read_sql(self, query: str) -> Union[pd.DataFrame, Iterator[pd.DataFrame], None]:
+        try:
+            with sqlite3.connect(self.path_db, timeout=15) as conn:
+                return pd.read_sql(query, conn)
+        except:
+            print("Query exception")
+            return None
+
+    def update(self, query: str) -> bool:
+        try:
+            with sqlite3.connect(self.path_db) as conn:
+                cur = conn.cursor()
+                cur.execute(query)
+                conn.commit()
+                return True
+        except:
+            print("Query exception")
+            return False
+
+    def delete_table(self, table: str) -> None:
+        with sqlite3.connect(self.path_db) as conn:
+            cursor = conn.cursor()
+            cursor.execute("DROP TABLE IF EXISTS " + table)
+            conn.commit()
+
+    def drop_database(self) -> None:
+        os.remove(self.path_db)
