0cee600d9a1133fd73db63dd4d666bcde45ab79c
veronicator <80213270+veronicator@users.noreply.github.com>
2022-12-15 17:15:07 +0100

    added methods to validate json from files in utility/json.py

--- PycharmProject/src/utility/json.py
+++ PycharmProject/src/utility/json.py
@@ -1,4 +1,6 @@
-import jsonschema
+import json
+from jsonschema import exceptions, validate
+from utility import get_project_folder
 
 
 def validate_json(json_data, schema) -> bool:
@@ -9,7 +11,41 @@ def validate_json(json_data, schema) -> bool:
     :return: True if json object is valid, False otherwise.
     """
     try:
-        jsonschema.validate(instance=json_data, schema=schema)
-    except jsonschema.exceptions.ValidationError:
+        validate(instance=json_data, schema=schema)
+    except exceptions.ValidationError:
         return False
     return True
+
+
+def validate_json_data_file(json_data, schema_filename):
+    """
+    Validate a json object against a json schema in a file
+    :param json_data: json object
+    :param schema_filename: name of the file containing the json schema
+    :return: True if json object is valid, False otherwise
+    """
+    schema_path = get_project_folder() + "/" + schema_filename
+
+    with open(schema_path, "r", encoding="UTF-8") as file:
+        json_schema = json.load(file)
+
+    return validate_json(json_data, json_schema)
+
+
+def validate_json_file_file(json_data_filename, schema_filename):
+    """
+    Validate a json file against a json schema in a file
+    :param json_data_filename: file containing the json object
+    :param schema_filename: file containing the json schema
+    :return: True if json object is valid, False otherwise
+    """
+    json_file_path = get_project_folder() + "/" + json_data_filename
+    schema_file_path = get_project_folder() + "/" + schema_filename
+
+    with open(json_file_path, "r", encoding="UTF08") as file:
+        json_data = json.load(file)
+
+    with open(schema_file_path, "r", encoding="UTF08") as file:
+        json_schema = json.load(file)
+
+    return validate_json(json_data, json_schema)
