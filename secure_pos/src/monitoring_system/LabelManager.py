9323d8e9ebdd76cfc69bbb7dfab10c09522f1eb1
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-01-06 11:10:14 +0100

    continue implementation of first part of workflow (receipt and storage of labels)

--- secure_pos/src/monitoring_system/LabelManager.py
+++ secure_pos/src/monitoring_system/LabelManager.py
@@ -1,4 +1,5 @@
 import os
+import threading
 
 import pandas as pd
 
@@ -12,6 +13,7 @@ class LabelManager:
     tot_labels_from_classifier = 0
     tot_labels_from_expert = 0
     storer = LabelStorer()
+    access_to_db = threading.Semaphore(1)
     pathLabel = os.path.join(get_received_data_folder(), 'label.json')
 
     def __int__(self, tot_labels_from_classifier, tot_labels_from_expert):
@@ -24,7 +26,9 @@ class LabelManager:
         else:
             self.tot_labels_from_expert += 1
 
-    def store_label(self):
+    def store_label(self, monitoring_window_length):
+        # blocco l'accesso al db
+        self.access_to_db.acquire()
         label = Label()
         label.load_from_file(self.pathLabel)
         label_dataframe = pd.DataFrame(label.to_dict(), index=[0])
@@ -34,3 +38,15 @@ class LabelManager:
         else:
             self.count_labels('expert')
             self.storer.store_label(label_dataframe, 'expertLabel')
+        if self.tot_labels_from_expert == monitoring_window_length or \
+                self.tot_labels_from_classifier == monitoring_window_length:
+            # avvio il monitoraggio su un altro thread
+            # in questo modo posso continuare a memorizzare label per il report successivo
+
+            # prima di procedere con il report
+            # caricare le etichette in memoria
+            # eliminarle dal db
+            # avviare il thread per il monitoraggio
+
+            pass
+        self.access_to_db.release()
--- secure_pos/src/monitoring_system/MonitoringSystemController.py
+++ secure_pos/src/monitoring_system/MonitoringSystemController.py
@@ -1,19 +1,25 @@
+import json
 import threading
 
 from communication import RestServer
 from communication.api.json_transfer import ReceiveJsonApi
 from monitoring_system.LabelManager import LabelManager
+from utility.json_validation import validate_json
 
 
 class MonitoringSystemController:
     label_manager = LabelManager()
+    config_path = "./conf/config.json"
+    config_schema_path = "./conf/config_schema.json"
+    config = None
 
     def __int__(self):
         self.label_manager = LabelManager()
 
     def handle_message(self):
         # When the system receives a message, generate a new thread
-        thread = threading.Thread(target=self.label_manager.store_label)
+        thread = threading.Thread(target=self.label_manager.store_label,
+                                  args=(self.config["monitoring_window_length"]))
         thread.start()
 
     def start_server(self):
@@ -37,7 +43,18 @@ class MonitoringSystemController:
         query = "CREATE TABLE classifierLabel (sessionId TEXT PRIMARY KEY UNIQUE, value TEXT)"
         self.label_manager.storer.create_table(query)
 
+    def load_config(self):
+        with open(self.config_path, "r", encoding="UTF-8") as file:
+            config = json.load(file)
+        with open(self.config_schema_path, "r", encoding="UTF-8") as file:
+            config_schema = json.load(file)
+        validate_json(config, config_schema)
+        self.config = config
+
     def run(self):
+        # carico la configurazione
+        self.load_config()
+
         # creo le tabelle per memorizzare le label
         self.create_tables()
 
