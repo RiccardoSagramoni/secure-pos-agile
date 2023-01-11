8e5f317e2820ffef0d937ef9daea3dfe3d88fdbe
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-01-11 16:38:44 +0100

    exception handler

--- secure_pos/src/preparation_system/prepared_session_sender.py
+++ secure_pos/src/preparation_system/prepared_session_sender.py
@@ -9,6 +9,9 @@ class PreparedSessionSender:
         self.url = url
 
     def send_prepared_session(self, prepared_session_dict):
-        response = requests.post(self.url, json=prepared_session_dict)
-        if not response.ok:
-            logging.error("Failed to send prepared session:\n%s", prepared_session_dict)
+        try:
+            response = requests.post(self.url, json=prepared_session_dict)
+            if not response.ok:
+                logging.error("Failed to send prepared session:\n%s", prepared_session_dict)
+        except requests.exceptions.RequestException as ex:
+            logging.error("Unable to send prepared session")
