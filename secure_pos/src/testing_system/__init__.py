1f633dbfc6980308b46d049c04a119e538d7c437
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-01-12 19:37:18 +0100

    Tester
    Co-Authored-By: Fedeperrone98 <92581085+Fedeperrone98@users.noreply.github.com>

--- secure_pos/src/testing_system/__init__.py
+++ secure_pos/src/testing_system/__init__.py
@@ -1,14 +1,15 @@
-from testing_system.tester import TestingSystem
+from testing_system.tester import DevelopmentTester
+from testing_system.tester_execution import ExecutionTester
 
 
 def development_testing():
-    test_system = TestingSystem()
-    num_sessions = [50, 100, 200, 300, 400, 500, 1000]
+    test_system = DevelopmentTester()
+    num_sessions = [1]
     test_system.start_development_testing(num_sessions)
 
 
 def execution_testing():
-    test_system = TestingSystem()
+    test_system = ExecutionTester()
     execution_len = 1000
     monitoring_len = 10
     num_sessions = [i * (execution_len + monitoring_len) for i in range(1, 5)]
@@ -16,6 +17,6 @@ def execution_testing():
 
 
 if __name__ == "__main__":
-    # development_testing()
-    execution_testing()
+    development_testing()
+    # execution_testing()
     print("FINE TESTING")
--- secure_pos/src/testing_system/tester_execution.py
+++ secure_pos/src/testing_system/tester_execution.py
@@ -12,6 +12,7 @@ import utility
 from communication import RestServer
 from communication.api.json_transfer import ReceiveJsonApi
 
+
 #################################################
 ###                 UTILITY
 #################################################
@@ -20,11 +21,20 @@ def replace_broken_label(label):
         label['label'] = "ATTACK"
     return label
 
+
 #################################################
 ###             TESTING SYSTEM
 #################################################
-class DevelopmentTester:
+class ExecutionTester:
     WINDOW_SIZE = 10
+    
+    start_timestamp_dict = {}
+    start_timestamp_lock = threading.RLock()
+    
+    diff_timestamp_list = []
+    diff_timestamp_lock = threading.RLock()  # lock for diff_timestamp_dict
+    
+    NUM_SESSIONS = 0
     
     # [COMMUNICATION] Testing -> toolchain
     ingestion_system_url = "http://127.0.0.1:8000"
@@ -51,16 +61,29 @@ class DevelopmentTester:
             self.network.iloc[start_index: start_index + window_size].to_dict(orient='records'),
             self.label.iloc[start_index + window_size - 1].to_dict()
         )
-
-
-
+    
     #################################################
     ###             COMMUNICATION
     #################################################
     def __handle_message(self, message: dict):
-        self.received_response = message
-        self.semaphore.release()
+        # Ottenere il timestamp di arrivo, leggere il dict per prendere il timestamp di partenza
+        # e scrivi la differenza in una lista globale
+        session_id = message["session_id"]
+        end_timestamp_str = message["timestamp"]
+        end_timestamp = datetime.strptime(end_timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+        diff_timestamp = end_timestamp - self.start_timestamp_dict[session_id]
+        diff = diff_timestamp.total_seconds()
 
+        with self.diff_timestamp_lock:
+            self.diff_timestamp_list.append(diff)
+        
+            self.received_response = message
+        
+            if len(self.diff_timestamp_list) >= self.NUM_SESSIONS:
+                self.semaphore.release()
+            
+        
+    
     def __start_rest_server(self):
         server = RestServer()
         server.api.add_resource(
@@ -72,14 +95,14 @@ class DevelopmentTester:
             }
         )
         server.run(port=1234)
-
-
-
-
+    
     #################################################
-    ###             DEVELOPMENT
+    ###                EXECUTION
     #################################################
-    def __development_send_raw_sessions(self, num_sessions=100):
+    def __execution_send_raw_sessions(self,
+                                      num_sessions,
+                                      execution_length,
+                                      monitoring_length) -> dict:
         for i in range(num_sessions):
             session_index = i % self.TOTAL_NUM_SESSIONS
             
@@ -88,11 +111,18 @@ class DevelopmentTester:
                 start_index=session_index * self.WINDOW_SIZE,
                 window_size=self.WINDOW_SIZE
             )
+            
             # Get session id
             session_id = label_data['event_id']
             label_data = replace_broken_label(label_data)
+            print(f"{session_index}) {label_data}")
+            
+            # Register start timestamp
+            with self.start_timestamp_lock:
+                self.start_timestamp_dict[session_id] = datetime.now()
+            
             # Prepare data to send
-            data = [
+            data_to_send = [
                 {
                     'session_id': session_id,
                     'type': 'commercial',
@@ -107,33 +137,40 @@ class DevelopmentTester:
                     'session_id': session_id,
                     'type': 'network',
                     'data': network_data
-                },
-                {
-                    'session_id': session_id,
-                    'type': 'label',
-                    'data': label_data
                 }
             ]
-            print(f"{session_index}) {label_data}")
+            label_to_send = {
+                'session_id': session_id,
+                'type': 'label',
+                'data': label_data
+            }
+            
             # Send data to ingestion system
-            for record in data:
+            for record in data_to_send:
                 try:
                     requests.post(self.ingestion_system_url, json=record)
                 except Exception as ex:
                     print(ex)
                     print(f"FAIL session_id: {session_id}")
                     break
+            
+            # Check if we must send the label
+            if (session_index % (execution_length + monitoring_length)) >= execution_length:
+                print(f"MONITORING {session_index}")
+                try:
+                    requests.post(self.ingestion_system_url, json=label_to_send)
+                except Exception as ex:
+                    print(ex)
+                    print(f"FAIL label session_id: {session_id}")
+                    break
             time.sleep(2)
     
-    
-    
-    #
-    def __run_development_testing(self, num_sessions=100):
+    def __run_execution_testing(self, num_sessions, execution_len, monitoring_len):
         # Get timestamp
         start_timestamp = datetime.now()
         
         # Send raw sessions
-        self.__development_send_raw_sessions(num_sessions)
+        self.__execution_send_raw_sessions(num_sessions, execution_len, monitoring_len)
         
         # Wait for HTTP message
         self.semaphore.acquire()
@@ -152,10 +189,10 @@ class DevelopmentTester:
             "diff": diff
         }
         result_df = pd.DataFrame(result_dict, index=[0])
-        result_df.to_csv("development.csv", sep=',', encoding="UTF-8", mode='a', header=False, index=False)
+        result_df.to_csv("execution.csv", sep=',', encoding="UTF-8", mode='a', header=False, index=False)
     
     #
-    def start_development_testing(self, num_session_list: list) -> None:
+    def start_execution_testing(self, num_session_list: list, execution_len, monitoring_len) -> None:
         # Create development.csv file
         with open("development.csv", "w") as file:
             file.write("scenario_id,diff\n")
@@ -163,12 +200,10 @@ class DevelopmentTester:
         # Start REST server
         flask_thread = threading.Thread(target=self.__start_rest_server, daemon=True)
         flask_thread.start()
-        time.sleep(5)
-    
+        
         for num in num_session_list:
-            self.__run_development_testing(num)
-    
+            self.NUM_SESSIONS = num
+            self.__run_execution_testing(num, execution_len, monitoring_len)
+        
         # todo do stuff here?
         return
-
-
